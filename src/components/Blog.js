e10af6a05167f5984e43ef188132dc656f827c52
amath vaillam <amathv01@gmail.com>
2020-08-11 12:00:49 +0000

    5.11: Blog list frontend, step11

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,26 +1,27 @@
 import React, { useState } from 'react'
-import Togglable from './Togglable'
-
+import PropTypes from "prop-types"
 const Blog = ({ blog, update, remove }) => {
 
   const [likes, setLikes] = useState(blog.likes)
-  const [url, setUrl] = useState(blog.url)
-  const [author, setAuthor] = useState(blog.author)
-  const [id, setId] = useState(blog.id)
   return (
     <div>
-      <div>{url}</div>
+      <div>{blog.url}</div>
       <div>likes {likes}  <button onClick={() => {
         setLikes(likes + 1)
         update({ ...blog, likes })
       }}>like</button></div>
-      <div>{author}</div>
+      <div>{blog.author}</div>
       <div><button style={{ backgroundColor: 'blue' }} onClick={() => {
         remove(blog)
       }}>remove</button>
       </div>
     </div>
   )
+}
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  update: PropTypes.func.isRequired,
+  remove: PropTypes.func.isRequired,
 }
 
 export default Blog
